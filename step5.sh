907114e9e21daf1f0e2480a21115627c5e7e3a3f
GuanhuaJi <jgh1013@seas.upenn.edu>
2025-10-01 19:41:22 -0400

    add example script

---
+++ step5.sh
@@ -0,0 +1,14 @@
+python /home/guanhuaji/oxeplusplus/sam2/load_dataset.py --dataset austin_sailor_dataset_converted_externally_to_rlds --save_dir /home/guanhuaji/load_datasets --start 0 --end 100
+python /home/guanhuaji/oxeplusplus/sam2/load_dataset.py --dataset austin_buds_dataset_converted_externally_to_rlds --start 0 --end 134 --save_dir /home/guanhuaji/load_datasets
+python /home/guanhuaji/oxeplusplus/sam2/load_dataset.py --dataset toto --start 0 --end 134 --save_dir /home/guanhuaji/load_datasets
+python /home/guanhuaji/oxeplusplus/sam2/load_dataset.py --dataset berkeley_autolab_ur5 --start 0 --end 134 --save_dir /home/guanhuaji/load_datasets
+python /home/guanhuaji/oxeplusplus/sam2/load_dataset.py --dataset utaustin_mutex --start 0 --end 134 --save_dir /home/guanhuaji/load_datasets
+python /home/guanhuaji/oxeplusplus/sam2/load_dataset.py --dataset taco_play --start 0 --end 134 --save_dir /home/guanhuaji/load_datasets
+python /home/guanhuaji/oxeplusplus/sam2/load_dataset.py --dataset language_table --start 0 --end 134 --save_dir /home/guanhuaji/load_datasets
+python /home/guanhuaji/oxeplusplus/sam2/load_dataset.py --dataset viola --start 0 --end 134 --save_dir /home/guanhuaji/load_datasets
+python /home/guanhuaji/oxeplusplus/sam2/load_dataset.py --dataset iamlab_cmu_pickup_insert_converted_externally_to_rlds --save_dir /home/guanhuaji/load_datasets
+python /home/guanhuaji/oxeplusplus/sam2/load_dataset.py --dataset utokyo_xarm_pick_and_place_converted_externally_to_rlds --save_dir /home/guanhuaji/load_datasets
+python /home/guanhuaji/oxeplusplus/sam2/load_dataset.py --dataset ucsd_kitchen_dataset_converted_externally_to_rlds --save_dir /home/guanhuaji/load_datasets
+python /home/guanhuaji/oxeplusplus/sam2/load_dataset.py --dataset kaist_nonprehensile_converted_externally_to_rlds --save_dir /home/guanhuaji/load_datasets
+python /home/guanhuaji/oxeplusplus/sam2/load_dataset.py --dataset bridge --save_dir /home/guanhuaji/load_datasets
+python /home/guanhuaji/oxeplusplus/sam2/load_dataset.py --dataset fractal20220817_data --save_dir /home/abrashid/OXE_inpainting
